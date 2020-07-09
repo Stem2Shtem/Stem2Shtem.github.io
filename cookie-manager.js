02f8ba85a44209353b4b69c7b5f3fb707851a818
Kyle Heller <kyleeheller@gmail.com>
2020-07-09 16:51:14 -0700

    Added Black hole effect to starmerchant_about_blackholes and linked up the sit

--- cookie-manager.js
+++ cookie-manager.js
@@ -16,8 +16,7 @@ function getCookie(cname) {
     var c=ca[i];
     while(c.charAt(0)==' ') {
       c = c.substring(1);
-    }
-	if(c.indexOf(name)==0) {
+    }if(c.indexOf(name)==0) {
       return c.substring(name.length,c.length);
     }
   }
